ee4e6f5be58d8d77a8714c6b175354fb784400ad
W0lFe <snapshit@sapo.pt>
2018-06-21 15:44:38 +0100

    Adicionado phpoffice e feitas a conversao e insercao de excel

--- mario/includes/InputDB.php
+++ mario/includes/InputDB.php
@@ -1,20 +1,25 @@
 <?php
 
+
+function putDB($name){
 include './includes/config.php'; //incluimos o ficheiro da ligacao
 $con = getdb();//os dados desta serao retornados desta funcao
-
 mysqli_set_charset($con, 'utf8'); //declaramos que a charset a usar e utf8
 set_time_limit(1000000);//e absurdamente aumentamos o tempo limite
- if(file_exists ('C:xampp/mysql/data/alocmf.csv')){
+$query="";
+ if(file_exists ('C:xampp/mysql/data/'.$name.'.csv')){
+	 $see="Select * from `alojamento`";
+	 $result=mysqli_query($con,$see);
+	 if(mysqli_num_rows($result)>=1)
+{
 	$del="Delete  from `alojamento`";//primeiro apagamos os dados ja existentes
 	$con->query($del);
-	$arr=mysqli_error_list($con); //se tiver erros na query esta variavel guardará-os a todos[em array]
-	if(!mysqli_affected_rows($con)){
-		echo "error";//no caso de nada ser mudado
-	}
-	else{
+}
+
+
+		if($name=='aloj'){
 		$query = <<<eof
-	LOAD DATA INFILE './alocmf.csv'
+	LOAD DATA INFILE './aloj.csv'
 	 INTO TABLE alojamento
 	  FIELDS TERMINATED BY ','
 	   OPTIONALLY ENCLOSED BY '"'
@@ -22,28 +27,32 @@ set_time_limit(1000000);//e absurdamente aumentamos o tempo limite
 		   IGNORE 1 LINES
 		   (numero_registo,Data_registo,Nome_Alojamento,Imovel_Posterior_1951,Data_Abertura_Publico,Modalidade,numero_camas,Numero_Utentes,numero_quartos,numero_beliches,Endereco,codigo_postal,Localidade,Freguesia,Concelho,Distrito,NUTT_II,Titular_da_Exploracao,Titular_Qualidade,Contribuinte,Tipo_Titular,Pais_Titular,Telefone,Fax,Telemovel,Email)
 eof;
+	}
+	else if($name=='hotel'){
+	$query = <<<eof
+	LOAD DATA INFILE './hotel.csv'
+	 INTO TABLE hotel
+	  FIELDS TERMINATED BY ','
+	   OPTIONALLY ENCLOSED BY '"'
+		   LINES TERMINATED BY '\r\n'
+		   IGNORE 1 LINES
+		   (numero_registo,Data_registo,Nome_Alojamento,Imovel_Posterior_1951,Data_Abertura_Publico,Modalidade,numero_camas,Numero_Utentes,numero_quartos,numero_beliches,Endereco,codigo_postal,Localidade,Freguesia,Concelho,Distrito,NUTT_II,Titular_da_Exploracao,Titular_Qualidade,Contribuinte,Tipo_Titular,Pais_Titular,Telefone,Fax,Telemovel,Email)
+eof;
+	}
+
 //na query iremos usar o estilo LOAD DATA CSV.O ficheiro csv sempre necessitará estar na pasta C:/xampp/mysql/data
 //nao conseguimos alterar o caminho,por mais que se especifique,ele sempre só procura dentro dessa pasta.Sem problemas.
 $con->query($query);
 $arr=mysqli_error_list($con);
 if(mysqli_affected_rows($con)>0){
-echo "<script type=\"text/javascript\">
-alert(\"Done.\");
-window.location = \"index.php\"
-</script>";		
+return "DONE";
 }
 else if(mysqli_affected_rows($con)==-1){
-	echo "<script type=\"text/javascript\">
-	alert(\"Error.\");
-	window.location = \"index.php\"
-	</script>";	
+	return "NOTINSERTED";
 }
 	}	 
-}
 else{
-	echo "<script type=\"text/javascript\">
-	alert(\"Error.\");
-	window.location = \"index.php\"
-	</script>";	
+	return "FILENOTFOUND";
+}
 }
  ?>
