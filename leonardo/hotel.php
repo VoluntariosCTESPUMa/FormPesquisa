316628659ac8e6d67d76cb3d5015b9132ad739b2
Fireballpt1 <32848841+Fireballpt1@users.noreply.github.com>
2018-06-24 21:38:36 +0100

    index mario direito

--- leonardo/hotel.php
+++ leonardo/hotel.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Alojamento</title>
+<title>Hotel</title>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="../style.css">
@@ -15,8 +15,8 @@
 
 <div class="topnav">
   <a href="./home.php">Turismo Madeira</a>
-  <a href="./hotel.php">Hoteis</a>
-  <a class="active" href="./index.php">Alojamentos</a>
+  <a class="active" href="./hotel.php">Hoteis</a>
+  <a  href="./alojamento.php">Alojamentos</a>
 </div>
 
 <div class="row" style="margin:2rem;">
@@ -24,22 +24,16 @@
 <div class="direitinho">
   <input onkeyup="myFunction()" class="form-control" id='myInput' type='text' style='width: 28%;height:35px;' placeholder='Search..'>
   <select class="form-control" style='width: 20%; margin-left: 10px; height:35px;' id="mySelect">
-        <option value="0">Numero de Registo</option>
-        <option value="1">Data do Registo</option>
-        <option value="2">Nome do Alojamento</option>
-        <option value="3">Imovél Posterior 1951</option>
-        <option value="4">Data Abertura Público</option>
-        <option value="5"> Modalidade</option>
-        <option value="6">Número de camas</option>
-        <option value="7">Número de Utentes</option>
-        <option value="8"> Número de Quartos </option>
-        <option value="9">Número de Beliches</option>
-        <option value="10">Localização(Endereço) </option>
-        <option value="11">Localização(Código de Postal)</option>
-        <option value="12">Localidade</option>
-        <option value="13">Freguesia</option>
-        <option value="14">Concelho</option>
-        <option value="15">Distrito </option>
+        <option value="0">IdHotel(BD)</option>
+        <option value="1">Categoria</option>
+        <option value="2">Classificação</option>
+        <option value="3">Unidade Hoteleira</option>
+        <option value="4">Diretor</option>
+        <option value="5">Diretor Comercial</option>
+        <option value="6">E-mail Diretor Comercial</option>
+        <option value="7">E-mail Geral</option>
+        <option value="8">Telefone</option>
+        <option value="9">Grupo Hoteleiro</option>
     </select>
     </div></div>
   <div class="column" >
@@ -49,7 +43,7 @@
   <div style="overflow-x:scroll; overflow-y:scroll; height:380px;">
   <table > 
   <meta charset="utf-8"/>
-  <?php include './includes/getalojamento.php';?>
+  <?php include './includes/gethotel.php';?>
 </table>
 </div>
   </div>
@@ -60,7 +54,6 @@
   <h4 style="text-align:center;background-color:#2196F3;color:white;height:60px;" ><br>Lista de Exportação <i class="fa fa-bars"></i></h4>
   <a href="#" style="display:none;" id="rem"  onclick="removeall()">Remover todos <i class="glyphicon glyphicon-file"></i> </a>
   <a href="#" style="display:none;" id="down">Exportar lista <i class="glyphicon glyphicon-file"></i> </a>
-  <br>
   <div class="column" style="overflow-y:scroll; width:100%;" >
 
   
@@ -166,7 +159,8 @@ function addall(){
       var linha={}; //criamos uma variavel local
       var t = document.getElementById("sourcetable");//pegamos no elemento da primeira tabela
       d = t.getElementsByTagName("tr");//pegamos na TR
-     var cabeca=["numero_registo","Data_registo","Nome_Alojamento","Imovel_Posterior_1951","Data_Abertura_Publico","Modalidade","numero_camas","Numero_Utentes","numero_quartos","numero_beliches","Endereco","codigo_postal","Localidade","Freguesia","Concelho","Distrito","NUTT_II","Titular_da_Exploracao","Titular_Qualidade","Contribuinte","Tipo_Titular","Pais_Titular","Telefone","Fax","Telemovel","Email"];  //defenimos as propriedades do objeto
+      
+     var cabeca=["IdHotel","categoria","classificacao","unidade_hoteleira","diretor","email_diretor","diretor_comercial","email_diretor_comercial","email_geral","telefone","grupo_hoteleiro"];  //defenimos as propriedades do objeto
       for(var i=1;i!=d.length;i++){
         linha={};//esvaziamos o objeto
           var temp=d[i].innerText.split("\t"); //agarramos nas trs das linhas seguintes
@@ -177,17 +171,13 @@ function addall(){
     var table = document.getElementById("replaceme");//inserimos esta nova entrada na lista do outro lado
     var row   = table.insertRow(-1);
     var cell1 = row.insertCell(-1);
-    cell1.innerHTML = '<table id="a"><tr><td onclick="tdclick(event,$(this))"><a style="color:red;" href="#" title="Clique aqui para remover esta entrada">'+linha['numero_registo']+'</a></td><td>'+linha['Data_registo']+'</td><td>'+linha['Nome_Alojamento']+'</td><td>'+linha['Imovel_Posterior_1951']+'</td><td>'+linha['Data_Abertura_Publico']+'</td><td>'+linha['Modalidade']+'</td><td>'+linha['numero_camas']+'</td><td>'+linha['Numero_Utentes']+'</td><td>'+linha['numero_quartos']+'</td><td>'+linha['numero_beliches']+'</td><td>'+linha['Endereco']+'</td><td>'+linha['codigo_postal']+'</td><td>'+linha['Localidade']+'</td><td>'+linha['Freguesia']+'</td><td>'+linha['Concelho']+'</td><td>'+linha['Distrito']+'</td><td>'+linha['NUTT_II']+'</td><td>'+linha['Titular_da_Exploracao']+'</td><td>'+linha['Titular_Qualidade']+'</td><td>'+linha['Contribuinte']+'</td><td>'+linha['Tipo_Titular']+'</td><td>'+linha['Pais_Titular']+'</td><td>'+linha['Telefone']+'</td><td>'+linha['Fax']+'</td><td>'+linha['Telemovel']+'</td><td>'+linha['Email']+'</td></tr></table><br>';    	         
-      
-    
+    cell1.innerHTML = '<table id="a"><tr><td onclick="tdclick(event,$(this))"><a style="color:red;" href="#" title="Clique aqui para remover esta entrada">'+linha['IdHotel']+'</a></td><td>'+linha['categoria']+'</td><td>'+linha['classificacao']+'</td><td>'+linha['unidade_hoteleira']+'</td><td>'+linha['diretor']+'</td><td>'+linha['email_diretor']+'</td><td>'+linha['diretor_comercial']+'</td><td>'+linha['email_diretor_comercial']+'</td><td>'+linha['email_geral']+'</td><td>'+linha['telefone']+'</td><td>'+linha['grupo_hoteleiro']+'</td></tr></table><br>';
       }
       var a= document.getElementById('down') //e mostramos os botoes de download e remover
        a.style.display="block";
        a= document.getElementById('rem')
        a.style.display="block";
-
     }
-
 
 var coll = document.getElementsByClassName("collapsible");
 var i;
@@ -234,7 +224,7 @@ function myFunction() {
         if(!(e.target.innerText=="undefined")){
         var a=e.target.innerText;//agarra o id do produto presente na primeira TD
         for(var i=0;i!=$('#replaceme table').length;i++){
-          if(a==arr[i]['numero_registo']){
+          if(a==arr[i]['IdHotel']){
           arr.splice(i,1) //quando encontra no array,dá splice
           break;//saimos do for
           }
@@ -275,7 +265,8 @@ function trclick(row){
 window.console&&console.log(linha);
       
       arr.push(linha);
-    cell1.innerHTML = '<table id="a"><tr><td onclick="tdclick(event,$(this))"><a style="color:red;" href="#" title="Clique aqui para remover esta entrada">'+linha['numero_registo']+'</a></td><td>'+linha['Data_registo']+'</td><td>'+linha['Nome_Alojamento']+'</td><td>'+linha['Imovel_Posterior_1951']+'</td><td>'+linha['Data_Abertura_Publico']+'</td><td>'+linha['Modalidade']+'</td><td>'+linha['numero_camas']+'</td><td>'+linha['Numero_Utentes']+'</td><td>'+linha['numero_quartos']+'</td><td>'+linha['numero_beliches']+'</td><td>'+linha['Endereco']+'</td><td>'+linha['codigo_postal']+'</td><td>'+linha['Localidade']+'</td><td>'+linha['Freguesia']+'</td><td>'+linha['Concelho']+'</td><td>'+linha['Distrito']+'</td><td>'+linha['NUTT_II']+'</td><td>'+linha['Titular_da_Exploracao']+'</td><td>'+linha['Titular_Qualidade']+'</td><td>'+linha['Contribuinte']+'</td><td>'+linha['Tipo_Titular']+'</td><td>'+linha['Pais_Titular']+'</td><td>'+linha['Telefone']+'</td><td>'+linha['Fax']+'</td><td>'+linha['Telemovel']+'</td><td>'+linha['Email']+'</td></tr></table><br>';    	         
+    cell1.innerHTML = '<table id="a"><tr><td onclick="tdclick(event,$(this))"><a style="color:red;" href="#" title="Clique aqui para remover esta entrada">'+linha['IdHotel']+'</a></td><td>'+linha['categoria']+'</td><td>'+linha['classificacao']+'</td><td>'+linha['unidade_hoteleira']+'</td><td>'+linha['diretor']+'</td><td>'+linha['email_diretor']+'</td><td>'+linha['diretor_comercial']+'</td><td>'+linha['email_diretor_comercial']+'</td><td>'+linha['email_geral']+'</td><td>'+linha['telefone']+'</td><td>'+linha['grupo_hoteleiro']+'</td></tr></table><br>';
+    
    // window.console&&console.log(linha);
         // var csv = linha['numero_registo'] + "\t" + linha['Data_registo'];
         // var data = new Blob([csv]);
@@ -300,7 +291,7 @@ window.console&&console.log(linha);
     
 		  var excel = $JExcel.new("Calibri light 10 #333333");			// Default font
 			
-			excel.set( {sheet:0,value:"Alojamentos" } );
+			excel.set( {sheet:0,value:"Hoteis" } );
 			
 			var evenRow=excel.addStyle ( { 																	// Style for even ROWS
 				border: "none,none,none,thin #333333"});													// Borders are LEFT,RIGHT,TOP,BOTTOM. Check $JExcel.borderStyles for a list of valid border styles
@@ -309,7 +300,7 @@ window.console&&console.log(linha);
 				border: "none,none,none,thin #333333"}); 
 			
  
-			var headers=["Número Registo", "Data Registo", "Nome do Alojamento", "Imovél Posterior 1951", "Data Abertura Público", "Modalidade", "Número de camas", "Número de Utentes", "Número de Quartos", "Número de Beliches", "Endereço", "Código de Postal", "Localidade", "Freguesia", "Concelho", "Distrito", "NUTT II","Titular_da_Exploracao","Titular_Qualidade","Contribuinte","Tipo_Titular","Pais_Titular","Telefone","Fax","Telemovel","Email"];							// This array holds the HEADERS text
+			var headers=["Categoria","Classificacao","Unidade Hoteleira","Diretor","Email Diretor","Diretor Comercial","Email Diretor Comercial","Email Geral","Telefone","Grupo Hoteleiro"];  //defenimos as propriedades do objeto							// This array holds the HEADERS text
       
       
       var formatHeader=excel.addStyle ( { 															// Format for headers
@@ -332,33 +323,17 @@ window.console&&console.log(linha);
         
         $.each(arr, function (index, value) {
 				//para cada item no array
-        
-          excel.set(0,0,index+1,arr[index]['numero_registo']);	
-          excel.set(0,1,index+1,arr[index]['Data_registo']);
-          excel.set(0,2,index+1,arr[index]['Nome_Alojamento']);
-          excel.set(0,3,index+1,arr[index]['Imovel_Posterior_1951']);															
-          excel.set(0,4,index+1,arr[index]['Data_Abertura_Publico']);
-          excel.set(0,5,index+1,arr[index]['Modalidade']);
-          excel.set(0,6,index+1,arr[index]['numero_camas']);
-          excel.set(0,7,index+1,arr[index]['Numero_Utentes']);
-          excel.set(0,8,index+1,arr[index]['numero_quartos']);
-          excel.set(0,9,index+1,arr[index]['numero_beliches']);
-          excel.set(0,10,index+1,arr[index]['Endereco']);
-          excel.set(0,11,index+1,arr[index]['codigo_postal']);
-          excel.set(0,12,index+1,arr[index]['Localidade']);
-          excel.set(0,13,index+1,arr[index]['Freguesia']);
-          excel.set(0,14,index+1,arr[index]['Concelho']);
-          excel.set(0,15,index+1,arr[index]['Distrito']);
-          excel.set(0,16,index+1,arr[index]['NUTT_II']);
-          excel.set(0,17,index+1,arr[index]['Titular_da_Exploracao']);
-          excel.set(0,18,index+1,arr[index]['Titular_Qualidade']);
-          excel.set(0,19,index+1,arr[index]['Contribuinte']);
-          excel.set(0,20,index+1,arr[index]['Tipo_Titular']);
-          excel.set(0,21,index+1,arr[index]['Pais_Titular']);
-          excel.set(0,22,index+1,arr[index]['Telefone']);
-          excel.set(0,23,index+1,arr[index]['Fax']);
-          excel.set(0,24,index+1,arr[index]['Telemovel']);
-          excel.set(0,25,index+1,arr[index]['Email']);
+          excel.set(0,0,index+1,arr[index]['categoria']);	
+          excel.set(0,1,index+1,arr[index]['classificacao']);
+          excel.set(0,2,index+1,arr[index]['unidade_hoteleira']);
+          excel.set(0,3,index+1,arr[index]['diretor']);		
+          excel.set(0,4,index+1,arr[index]['email_diretor']);	
+          excel.set(0,5,index+1,arr[index]['diretor_comercial']);	
+          excel.set(0,6,index+1,arr[index]['email_diretor_comercial']);	
+          excel.set(0,7,index+1,arr[index]['email_geral']);	
+          excel.set(0,8,index+1,arr[index]['telefone']);	
+          excel.set(0,9,index+1,arr[index]['grupo_hoteleiro']);														
+
           var d=randomDate(initDate,endDate);															
           excel.set(0,1,d.toLocaleString());														
           excel.set(0,2,$JExcel.toExcelLocalTime(d));											
@@ -367,7 +342,7 @@ window.console&&console.log(linha);
          								
         })	
 					
-        excel.generate("ALOJAMENTOS.xlsx");
+        excel.generate("HOTEL.xlsx");
       
 			
 		};
